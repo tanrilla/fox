eff1c4f7090d3a7b9af8b7a088b0818a3d70cbbb
Cesar <cesar.kina@gmail.com>
2020-05-27 20:23:21 -0500

    Initial import: fox-security

---
+++ fox-security/src/main/java/com/tanrilla/fox/FoxSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.tanrilla.fox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxSecurityApplication.class, args);
+	}
+
+}
---
+++ fox-security/src/test/java/com/tanrilla/fox/FoxSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanrilla.fox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
