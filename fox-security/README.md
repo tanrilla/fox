e4d1b6856831945c262f248d680a804747f86147
Cesar <cesar.kina@gmail.com>
2020-05-29 20:03:13 -0500

    WIP: Spring security+redis

--- fox-security/README.md
+++ fox-security/README.md
@@ -1,4 +1,9 @@
 # fox-security
 
+Start redis
+```bash
+docker-compose up
+```
+
 References:
 - https://spring.io/guides/tutorials/spring-security-and-angular-js/
--- fox-security/src/main/java/com/tanrilla/fox/FoxSecurityApplication.java
+++ fox-security/src/main/java/com/tanrilla/fox/FoxSecurityApplication.java
@@ -2,14 +2,18 @@ package com.tanrilla.fox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.security.Principal;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -38,11 +42,18 @@ public class FoxSecurityApplication {
 		return model;
 	}
 
+	@RequestMapping("/token")
+	public Map<String,String> token(HttpSession session) {
+		return Collections.singletonMap("token", session.getId());
+	}
+
 	@Configuration
 	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http
+					.cors()
+					.and()
 					.httpBasic()
 					.and()
 					.authorizeRequests()
@@ -52,6 +63,11 @@ public class FoxSecurityApplication {
 					.and().csrf()
 					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());;
 		}
+	}
+
+	@Bean
+	HeaderHttpSessionStrategy sessionStrategy() {
+		return new HeaderHttpSessionStrategy();
 	}
 
 	public static void main(String[] args) {
